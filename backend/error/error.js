a65a324a53e3e5e548d8a8478200392fb2c5137b
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-05 02:23:55 +0530

    .

--- backend/error/error.js
+++ backend/error/error.js
@@ -8,6 +8,7 @@ class ErrorHandler extends Error {
 export const errorMiddleware =(err,req,res,next) => {
     err.message=err.message || "internal server error !";
     err.statusCode=err.statusCode || 500;
+    
 
     return res.status(err.statusCode).json({
         success:false,

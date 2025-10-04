ad306ff1deeaf960d58251f7742d7d1e3376e651
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-05 00:45:52 +0530

    reaervation schema and backend

---
+++ backend/models/reservationSchema.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const reservationSchema=new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+        minLength:[3,"first name must contain at least 3 charecter"],
+        maxLength:[3,"first name cannot excced 30 cahrecters"],
+    },
+
+    lastName:{
+        type:String,
+        required:true,
+        minLength:[3,"first name must contain at least 3 charecter"],
+        maxLength:[3,"first name cannot excced 30 cahrecters"],
+    },
+
+    email: {
+        type:String,
+        required:true,
+        validate:[validator.isEmail,"provide a valid email"],
+    },
+
+    phone: {
+        type:Number,
+        required:true,
+        minLength:[10,"phone number must contain only 11 digits"],
+        maxLength:[10,"phone number must contain only 11 digits"],
+    },
+    time:{
+        type:String,
+        required:true,
+    },
+    date:{
+        type:String,
+        required:true,
+    },
+
+});
+
+export const Reservation=mongoose.model("Reaservation",reservationSchema);

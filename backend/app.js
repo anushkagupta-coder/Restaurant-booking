92efd5f43e1b959a8c936fafb9da9178ea0af5aa
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-04 23:32:26 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,4 +4,14 @@ import dotenv from "dotenv";
 
 const app = express()
 dotenv.config({path:"./config/config.env"});
+//.use for middleware
+app.use(cors({
+    origin:[process.env.FRONTEND_URL],
+    methods:["POST"],
+    credentials:true
+}))
+//string ko json obj me convert krdega
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 export default app;

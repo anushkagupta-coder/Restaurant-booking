64f3656777addb74d3b13fe45a0ed81ae4acf3e8
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-05 15:00:53 +0530

    Update Home.jsx

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import About from '../components/About.jsx';
 import Qualities from '../components/Qualities.jsx';
 import Menu from '../components/Menu.jsx';
 import WhoAreWe from '../components/WhoAreWe.jsx';
+import Team from '../components/Team.jsx';
 const Home = () => {
   return (
     <>
@@ -12,6 +13,7 @@ const Home = () => {
       <Qualities/>
       <Menu/>
       <WhoAreWe/>
+      <Team/>
       
     </>
   )

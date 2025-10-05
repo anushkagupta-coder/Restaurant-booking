aa3a8f2b21a9df63867be21bebd31407735b68df
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-05 12:47:35 +0530

    Update Home.jsx

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import HeroSection from '../components/HeroSection.jsx';
 import About from '../components/About.jsx';
 import Qualities from '../components/Qualities.jsx';
-import Qualities from '../components/Qualities.jsx';
+import Menu from '../components/Menu.jsx';
 const Home = () => {
   return (
     <>

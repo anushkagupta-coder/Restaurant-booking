6a16e20df799b2386ee17e8254ea90e179511eb0
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-05 11:41:18 +0530

    stylescss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import "./App.css";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import {Toaster} from 'react-hot-toast';
-import Home from "./Pages/Home";
-import Success from "./Pages/Success";
-import NotFound from "./Pages/NotFound";
+import Home from "./Pages/Home.jsx";
+import Success from "./Pages/Success.jsx";
+import NotFound from "./Pages/NotFound.jsx";
 const App = () => {
-  return 
+  return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/success" element={<Success/>} />
         <Route path="*" element={<NotFound/>} />
       </Routes>
-    </Router>;
-  
-}
+    </Router>
+  );
+};
 
-export default App
+export default App;

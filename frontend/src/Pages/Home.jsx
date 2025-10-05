739db393e76ab16cb7f24e73982e3c81faa38ed9
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-05 12:47:00 +0530

    Menu

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection.jsx';
 import About from '../components/About.jsx';
+import Qualities from '../components/Qualities.jsx';
 
 const Home = () => {
   return (
     <>
       <HeroSection/>
       <About/>
+
+      <Qualities/>
     </>
   )
 }

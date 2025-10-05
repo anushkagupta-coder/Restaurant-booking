13843d5adea857ee7faa8059a1a6efe9c2bbc675
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-05 14:50:02 +0530

    .

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -8,7 +8,6 @@ const Home = () => {
     <>
       <HeroSection/>
       <About/>
-
       <Qualities/>
       <Menu/>
 
---
+++ frontend/src/components/WhoAreWe.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {data} from "../restApi.json";
+const WhoAreWe = () => {
+  return (
+    <section className='wgo_are_we' id='who_are_we'>
+        <div className="container">
+            <div className="text_banner">
+                {
+                    data[0].who_we_are.map(element=> {
+                        return(
+                            <div className="card" key={element.id}>
+                                <h1 style={{fontWeight:"300"} } className='heading'>{element.number}</h1>
+                                <p>{element.title}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+      
+    </section>
+  )
+}
+
+export default WhoAreWe

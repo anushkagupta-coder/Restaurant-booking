a2fd6516f126211c8649c1887d4db79a9abd3545
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-05 12:33:45 +0530

    Update About.jsx

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -11,10 +11,10 @@ const About = () => {
                     <p>We love serving delicious food</p>
                 </div>
                 <p className='mid'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus praesentium nulla mollitia? Dicta quibusdam
-                     reprehenderit iusto libero iste, ducimus porro unde laboriosam culpa laudantium alias velit vel officiis dolore 
-                     consequatur cupiditate autem minima qui id nam corporis. Ducimus, nobis. Omnis beatae quis accusantium laboriosam debitis 
-                     et voluptate numquam cupiditate quibusdam!
+                    You love food? And guess what we love making it for you. 
+                    I am a passionate Youtuber and a mother of 2 lovely children and Now have my own restraunt.
+                    The best Cuisine you will ever have is waiting for you. 
+                    We are happy having you here .What are you waitng for have your reservations NOWWWW!!!!
                 </p>
                 <Link to={"/"}>
                     Explore Menu{""} 

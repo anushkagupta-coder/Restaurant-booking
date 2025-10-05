6c12357cfc26044c838468d510a1cbdfde32ef2c
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-05 16:14:17 +0530

    Update Success.jsx

--- frontend/src/Pages/Success.jsx
+++ frontend/src/Pages/Success.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
+import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
+import { useNavigate } from 'react-router-dom';
+import Link from "react-router-dom";
 
 const Success = () => {
+    const [countdown,setCountdown]=useState(10);
+    const navigate=useNavigate();
+
+    useEffect(()=>{
+        const timeoutId=setInterval(()=>{
+            setCountdown(preCount=>{
+                if(preCount ===1){
+                    clearInterval(timeoutId)
+                    navigate("/")
+                }
+                return precount - 1;
+            });
+        },1000);
+        return ()=>clearInterval(timeoutId);
+    },[navigate]);
   return (
     <>
-      <h1>Booking Successful!</h1>  
+    <section className='notFound'>
+        <div className="container">
+            <img src="/sandwich.png" alt="success" />
+            <h1>redirecting to Home in {countdown} seconds...</h1>
+            <Link to ={'/'}>Back to Home <HiOutlineArrowNarrowRight/></Link>
+        </div>
+        
+        
+    </section> 
     </>
   )
 }

12445ef687645004462ba1cba2058f74117ba344
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-05 16:03:47 +0530

    Update NotFound.jsx

--- frontend/src/Pages/NotFound.jsx
+++ frontend/src/Pages/NotFound.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import Link from "react-router-dom"
+import {HiOutlineArrowNarrowRight} from "react-icons/hi"
 const NotFound = () => {
   return (
     <>
-      <h1>Page Not Found</h1>
+      <section className='notFound'>
+        <div className="container">
+            <img src="/notFound.svg" alt="notFound" />
+            <h1>Looks Like You're Lost</h1>
+            <p>We can't find the page you are looking for </p>
+            <Link to={'/'}>Back to Home <span>
+                <HiOutlineArrowNarrowRight/>
+                </span>
+            </Link>
+        </div>
+      </section>
     </>
   )
 }
